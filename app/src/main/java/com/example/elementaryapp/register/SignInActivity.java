68a11312735bd133362c49e299b445f926656294
DItheM <d.ipalawatta@gmail.com>
2024-01-31 22:51:58 +0530

    home layout added

--- app/src/main/java/com/example/elementaryapp/register/SignInActivity.java
+++ app/src/main/java/com/example/elementaryapp/register/SignInActivity.java
@@ -2,9 +2,13 @@ package com.example.elementaryapp.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.elementaryapp.R;
+import com.example.elementaryapp.content.HomeActivity;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button startLearningBtn = findViewById(R.id.start_learning_btn);
+
+        startLearningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
